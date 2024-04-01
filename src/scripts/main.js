651ab3f7a38923d900e36dd715d1d7f995ec47ad
Khausta <anna.a.khaustova@gmail.com>
2024-04-01 16:22:18 +0300

    first commit

---
+++ src/scripts/main.js
@@ -0,0 +1,20 @@
+
+// const height = document.querySelector('.box-item_2'); //высота блока с меню
+// console.log(height.scrollHeight);
+// const el = document.querySelector('.blue'); //высота меню
+// console.log(el.scrollHeight);
+
+// var controller = new ScrollMagic.Controller();
+
+// // build scene
+// var scene = new ScrollMagic.Scene({triggerElement: "#trigger1", duration: height.scrollHeight})
+// .setPin("#pin1" )
+// // .scrollOffset()
+// .triggerHook('onLeave')
+// .addIndicators({name: "1 (duration: 300)"}) // add indicators (requires plugin)
+// .addTo(controller);
+
+// const btn = document.querySelector('.button');
+// btn.addEventListener('mouseover', () => {
+//     console.log('over');
+// })
